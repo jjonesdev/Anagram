3da691178b910b192416a689e5814d562d0310c1
Jordan <jordan.jones@detroitlabs.com>
2018-08-31 10:32:23 -0400

    Annnnd I forgot to push, program is mostly working. Checking for the anagram is working, now I need to add the alerts

--- Anagram/ViewController.swift
+++ Anagram/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var anagramLabel: UILabel!
+    @IBOutlet weak var anagramTextfield: UITextField!
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    let anagramWord = "Supercalifragilisticexpialidocious"
+    var score = 0
+    var isAnagram = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        anagramLabel.text = getAnagram(anagram: anagramWord)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func checkAnagram(_ sender: UIButton) {
+        for letter in anagramTextfield.text! {
+            if anagramWord.contains(letter) {
+                isAnagram = true
+            } else {
+                isAnagram = false
+            }
+        }
+        print(isAnagram)
+        if isAnagram == true {
+            score += 1
+        } else {
+            score -= 1
+        }
+        
+        scoreLabel.text = "Score: \(score)"
+        anagramTextfield.text = ""
+    }
+    
+    func getAnagram(anagram: String) -> String {
+        return anagram
+    }
+    
 }
 

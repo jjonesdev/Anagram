26803b482c45e77173507ae1b20639442584aed8
Jordan <jordan.jones@detroitlabs.com>
2018-09-02 13:42:00 -0400

    Words class is reading my test textfile and adding them to my dictionary, but it is not seperating each string by a new line

--- Anagram/ViewController.swift
+++ Anagram/ViewController.swift
@@ -23,7 +23,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        loadWords()
+        Words().loadWords()
         anagramLabel.text = getAnagram(anagram: anagramWord)
         
     }
